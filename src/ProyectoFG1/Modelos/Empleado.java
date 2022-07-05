efffb0fe530f307f8660034140fab69a232ad9de
Diegassus <diegomdiaz53@gmail.com>
2022-07-05 17:44:04 -0300

    clase Empleado

---
+++ src/ProyectoFG1/Modelos/Empleado.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProyectoFG1.Modelos;
+
+/**
+ *
+ * @author Tincho
+ */
+public class Empleado {
+    
+}
